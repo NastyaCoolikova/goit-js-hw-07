e90930f37234a442e092f7a727ed73e3fdddf1e1
NastyaCoolikova <111916229+NastyaCoolikova@users.noreply.github.com>
2023-02-10 20:10:04 +0100

    finished work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//import * as basicLightbox from 'basiclightbox'
+
+const galleryEl = document.querySelector('.gallery');
+
+function newGallery(image)  {
+    return image
+    .map (({preview, original, description}) =>{
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    })
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+
+
+
+document.querySelectorAll('.gallery__link').forEach(link => {
+    link.addEventListener('click', (event)=>{
+        const instance = basicLightbox.create(`<img src="${link.href}">`)
+    
+        instance.show()
+    
+        event.preventDefault();
+    })
+})
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+function newGallery(image)  {
+    return image
+    .map (({preview, original, description}) =>{
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    })
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
